8cbba751e0bd82add91d04e700244038c9847b65
Erica <ririkaganda7@gmail.com>
2024-01-13 18:46:39 +0800

    change

--- client/src/axiosHttp.js
+++ client/src/axiosHttp.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8081/server/api/',
+    baseURL: 'http://127.0.0.1:8000/api/',
     // headers: {
     //     common: {
     //       'key': '49aed241914cb37562cc025d7e2d3e4c', // Add your default Authorization header here
